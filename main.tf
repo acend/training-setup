f272ab9abff72c54db7ab9acbc95df14f9bb692a
Sebastian Plattner <plattner@puzzle.ch>
2023-03-30 17:44:12 +0200

    training-clister from main branch

--- main.tf
+++ main.tf
@@ -13,18 +13,19 @@ terraform {
 
 
 # module "training-cluster" {
-#   source = "git::https://github.com/acend/terraform-k8s-cluster-lab.git//modules/training-cluster?ref=move-to-hetzner"
+#   source = "git::https://github.com/acend/terraform-k8s-cluster-lab.git//modules/training-cluster"
 
 #   first_install = true
 
 #   cluster_name = "test"
 #   worker_count = "2"
 
-#   hcloud_api_tokens     = var.hcloud_api_token
+#   hcloud_api_token     = var.hcloud_api_token
 #   hosttech_dns_token   = var.hosttech_dns_token
 #   hosttech-dns-zone-id = var.hosttech_dns_zone_id
 
 
+#   # SSH Public keys deployed on the VM's for SSH access
 #   extra_ssh_keys = [
 #     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBH2Z8xf19GNAHY3t/S4kIVTcjDpUI83DG51vG48j8UOM0HodNcH8DnhvLb7WUptNtB056pNFSO5NDLgwa+paZbRzjg/9R5eWAM1q2aBYUrGqJTC2zP7BapA+50oEtKoPYTxQadkMCnEX+C8+Bk12aMPPWuFn85ARrmfA4jpqCbqj5iPzVCKIAKToTJAVM2nnCNpF8I2rHczd6QrZyXbGCMUYqR+Cs+cvcDGxzvew954zUkSRfOoeEfTgWqUQ8bbcL8Y9VZnhcFHCphSKCoFzu/MDlEZzAfnlcjqShUb0Quxo1DX3XaZUnH4rDL3Shnzyf2GQd9RL3cc0mA7DcqfYd sebastian@acend.ch",
 #     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDVSSqdclkvOOeqvMwxILmIZYcNxLSJ2Mzu3ep5XKa6SaM9el1Qy2p/hDCLCcxSnd09zraRC0v17Vw28i4XVSMMliEQAaWfnKYNOlt7y0/zVMz0zNk7gxeANQMCu8SSMqo9sbJtLoqkUHROc+xG0p2gYWvoczQhMUrgyPLCax720nginO1rsJEcA9W6ICp5nyAQ8ZUlpiBIGOhbMj36UvD3f2gsjx1aF6UMKkYYTLDXEb7OvwyyQkxJjBWY/C4OWS+0nHGArjqdpUMqBQvVGN+5j27a/Nn/Z7cK/X39CELAn1QwC6x3OFzn4gap6kUA1y3n2IZXUUMX9n+IgfvY4Di+ozy/hmM12W2kF9Wp6VzAdMwznW178EJ9fOOoycRYFyQrUOE/kJr+BOfVr1YLxtMYBRQrR6zjET1nrGD/Fjlgv/Xj3jOgOfXl0i0TZXlP0auV30tFxiPac+Z5j42s8Ijl7R+K4puu+gvwVkF4fuly0WR1RSG5z5K6gjd3CxrcYF8= daniel@acend.ch",
