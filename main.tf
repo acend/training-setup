1358b1892cfd74eb7b41965caf8a95c6de0443e3
Sebastian Plattner <plattner@puzzle.ch>
2023-08-11 20:22:04 +0200

    scale more

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ module "training-cluster" {
 
   cluster_name   = "training"
   cluster_domain = "cluster.acend.ch"
-  worker_count   = "8"
+  worker_count   = "12"
 
   hcloud_api_token     = var.hcloud_api_token
   hosttech_dns_token   = var.hosttech_dns_token
