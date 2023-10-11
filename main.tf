360c89f03650ad296e114e9f33c7cb0b557319d0
Raffael H <raffael@rokt.cloud>
2023-10-11 11:01:11 +0200

    Increase user count

--- main.tf
+++ main.tf
@@ -64,10 +64,10 @@ module "training-cluster" {
   # SSH Public keys deployed on the VM's for SSH access
   extra_ssh_keys = local.ssh_keys
 
-  cluster_admin = ["user1"]
+  cluster_admin = ["user1", "user2", "user3", "user4", "user5"]
 
   # Webshell
-  count-students = 1
+  count-students = 5
   # User VMs
   user-vms-enabled = false
 
