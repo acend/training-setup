db11e810846daf2d21e9064ed4029b142bab102a
Sebastian Plattner <sebastian.plattner@gmail.com>
2024-11-07 13:57:29 +0100

    use hcloud s3

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 terraform {
   backend "s3" {
-    bucket = "terraform"
-    key    = "terraform.tfstate"
-    region = "main"
+    bucket = "acend-s3-storage"
+    key    = "training-cluster/terraform.tfstate"
+    region = "nbg1"
 
+    skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    skip_region_validation      = true
-    force_path_style            = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
 
