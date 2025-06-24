6b5f5d05e22bd2f5ddb671254402eb424b489433
Daniel S <sybnex@gmail.com>
2025-06-24 18:14:10 +0200

    chore: fix helm to provider 2
    Signed-off-by: Daniel S <sybnex@gmail.com>

--- versions.tf
+++ versions.tf
@@ -6,6 +6,22 @@ terraform {
     hcloud = {
       source = "hetznercloud/hcloud"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "< 3.0.0"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+    time = {
+      source = "hashicorp/time"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
   }
 
   required_version = ">=1.9.0"
