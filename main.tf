cd57d89b477a57fa24a15303abc97338408f73be
Daniel S. <sybnex@gmail.com>
2026-06-12 11:10:01 +0200

    chore: rebuild cluster for testing reasons
    Signed-off-by: Daniel S. <sybnex@gmail.com>

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ module "training-cluster" {
   cluster_name       = "training"
   cluster_domain     = "cluster.acend.ch"
   controlplane_count = 3
-  worker_count       = "1" // A minimum of 3 nodes is required
+  worker_count       = 1 // 1 worker could host 4 users
 
   hcloud_api_token     = var.hcloud_api_token
   hosttech_dns_token   = var.hosttech_dns_token
